db6301bde69ffd1b83fe36c263820100fbd3b0f2
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2025-12-05 15:59:46 +0000

    Updated Track test

--- src/openutm_verification/core/clients/flight_blender/flight_blender_client.py
+++ src/openutm_verification/core/clients/flight_blender/flight_blender_client.py
@@ -23,6 +23,8 @@ from openutm_verification.models import (
     OperationState,
     SDSPHeartbeatMessage,
     SDSPSessionAction,
+    SDSPTrackMessage,
+    TrackMessage,
 )
 from openutm_verification.rid import (
     UASID,
@@ -599,14 +601,19 @@ class FlightBlenderClient(BaseBlenderAPIClient):
         ws = self.create_websocket_connection(endpoint=endpoint)
         return ws
 
+    def initialize_track_websocket_connection(self, session_id: str) -> Any:
+        endpoint = f"/ws/surveillance/track/{session_id}"
+        ws = self.create_websocket_connection(endpoint=endpoint)
+        return ws
+
     @scenario_step("Verify SDSP Track")
     def initialize_verify_sdsp_track(
         self,
-        expected_heartbeat_interval_seconds: int,
-        expected_heartbeat_count: int,
+        expected_track_interval_seconds: int,
+        expected_track_count: int,
         session_id: str,
     ) -> StepResult:
-        ws_connection = self.initialize_heartbeat_websocket_connection(session_id=session_id)
+        ws_connection = self.initialize_track_websocket_connection(session_id=session_id)
         start_time = time.time()
 
         now = arrow.now()
@@ -618,23 +625,24 @@ class FlightBlenderClient(BaseBlenderAPIClient):
             time.sleep(0.1)
             message = ws_connection.recv()
             message = json.loads(message)
-            if "heartbeat_data" not in message:
+            print(message)
+            if "track_data" not in message:
                 logger.debug("WebSocket connection established message received")
                 continue
-            heartbeat_message = HeartbeatMessage(**message["heartbeat_data"])
+            track = TrackMessage(**message["track_data"])
             logger.debug(f"Received WebSocket message: {message}")
-            all_received_messages.append(SDSPHeartbeatMessage(message=heartbeat_message, timestamp=arrow.now().isoformat()))
+            all_received_messages.append(SDSPTrackMessage(message=track, timestamp=arrow.now().isoformat()))
         logger.info(f"Received {len(all_received_messages)} messages in the first six seconds")
         self.close_heartbeat_websocket_connection(ws_connection)
         end_time = time.time()
-
+        duration = end_time - start_time
         # Sort messages by timestamp
         sorted_messages = sorted(all_received_messages, key=lambda msg: arrow.get(msg.timestamp))
         # remove the first two messages since we are just starting
         sorted_messages = sorted_messages[2:]
         all_message_intervals = []
         # Calculate and print intervals between consecutive messages
-        logger.debug("Heartbeat message intervals:")
+        logger.debug("Track message intervals:")
         for i in range(1, len(sorted_messages)):
             prev_timestamp = arrow.get(sorted_messages[i - 1].timestamp)
             curr_timestamp = arrow.get(sorted_messages[i].timestamp)
@@ -645,30 +653,21 @@ class FlightBlenderClient(BaseBlenderAPIClient):
             )
 
         verification_passed = True
-        tolerance = 0.1 * expected_heartbeat_interval_seconds  # Allow 10% tolerance
 
-        for interval in all_message_intervals:
-            if not (expected_heartbeat_interval_seconds - tolerance <= interval <= expected_heartbeat_interval_seconds + tolerance):
-                verification_passed = False
-                logger.debug(f"Interval {interval:.2f}s is outside the expected range.")
-                break
-        duration = end_time - start_time
         if not verification_passed:
-            logger.error(f"Heartbeat frequency verification failed: messages not received every {expected_heartbeat_interval_seconds} seconds")
+            logger.error(f"Track frequency verification failed: messages not received every {expected_track_interval_seconds} seconds")
             return StepResult(
-                name=f"Heartbeat message received {expected_heartbeat_interval_seconds} seconds",
+                name=f"Track message received {expected_track_interval_seconds} seconds",
                 status=Status.FAIL,
-                details=f"Heartbeat message not processed {expected_heartbeat_interval_seconds} seconds",
+                details=f"Track message not processed {expected_track_interval_seconds} seconds",
                 duration=duration,
             )
         else:
-            logger.info(
-                f"Heartbeat frequency verification passed: messages received approximately every {expected_heartbeat_interval_seconds} seconds"
-            )
+            logger.info(f"Track frequency verification passed: messages received approximately every {expected_track_interval_seconds} seconds")
             return StepResult(
-                name=f"Heartbeat message received {expected_heartbeat_interval_seconds} seconds",
+                name=f"Track message received {expected_track_interval_seconds} seconds",
                 status=Status.PASS,
-                details=f"Heartbeat message processed {expected_heartbeat_interval_seconds} seconds",
+                details=f"Track message processed {expected_track_interval_seconds} seconds",
                 duration=duration,
             )
 
